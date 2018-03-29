f79ead51b6bc6dffaaecb6fb66e6327c3ddf4f59
root <pengfei.xu@intel.com>
2018-03-29 10:16:50 +0800

    pdf.sh: update auto clean old pdf to bak folder function
    Signed-off-by: root <pengfei.xu@intel.com>

--- pdf.sh
+++ pdf.sh
@@ -3,9 +3,15 @@ file_name=""
 date=$(date +"%Y%m%d_%H%M")
 file_name=$1
 file=""
+bak="bak"
+
 if [[ -z "$file_name" ]]; then
-  enscript tbt_cases.sh -o - | ps2pdf - tbt_cases_$date.pdf
+  [[ -e "$bak" ]] || mkdir "$bak"
+  mv ./*.pdf "$bak"
+  enscript tbt_cases.sh -o - | ps2pdf - tbt_cases_"$date".pdf
+  sleep 1
+  enscript tbt_cases.sh -o - | ps2pdf - tbt_cases_in_DDT.pdf
 else
-  file=$(echo $file_name | cut -d '.' -f 1)
-  enscript $file_name -o - | ps2pdf - "$file"_"$date".pdf
+  file=$(echo "$file_name" | cut -d '.' -f 1)
+  enscript "$file_name" -o - | ps2pdf - "$file"_"$date".pdf
 fi
