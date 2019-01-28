82b7f8883a2c00f3e54fca96d5169b037da661b8
Pengfei Xu <pengfei.xu@intel.com>
2019-01-28 05:37:52 -0500

    compare.sh: improve the compare.sh
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- compare.sh
+++ compare.sh
@@ -1,69 +1,98 @@
 #!/bin/bash
 
-declare -A usb3_dict
-LOG_PATH="/opt/logs/5.0-rc1"
+LOG_PATH="/root/result"
 
-bounce_usb3="$LOG_PATH/tbt_iommu_bounce_usb3.txt"
-nobounce_usb3="$LOG_PATH/tbt_iommu_nobounce_usb3.txt"
+bounce_usb3="$LOG_PATH/iommu_b.txt"
+nobounce_usb3="$LOG_PATH/iommu_nb.txt"
+nbs_usb3="$LOG_PATH/iommu_nbs.txt"
+read_file="read.csv"
+write_file="write.csv"
+num=0
+usb3_infos=$(cat $bounce_usb3 | awk -F 'sent' '{print $2}' | awk -F 'MB' '{print $1}')
 
-IFS=","
-
-#declare -a usb3_infos="'1.00 MB' '8.00 MB' '64.00 MB' '256.00 MB' '1.00 GB'"
-usb3_infos=("1.00 MB","8.00 MB","64.00 MB","256.00 MB","1.00 GB")
+usb3=""
 usb3_info=""
-usb3_dict=(["1.00 MB"]="1k*1024" ["8.00 MB"]="8k*1024" ["64.00 MB"]="64k*1024" ["256.00 MB"]="256k*1024" ["1.00 GB"]="1024k*1024")
+
 rb_value=""
 rnb_value=""
-read_gap=""
-read_gap_rate==""
+rnbs_value=""
+
 wb_value=""
 wnb_value=""
-write_gap=""
-write_gap_rate==""
-read_result=""
-write_result=""
+wnbs_value=""
+
 rb_total=0
 rnb_total=0
+rnbs_total=0
+
 wb_total=0
 wnb_total=0
+wnbs_total=0
+
+rnb_gap=""
 rb_gap=""
+rnb_gap_rate=""
 rb_gap_rate=""
 
-echo "usb3_size:      usb3_nobounce,     usb3_bounce,    gap_rate"
+wb_gap=""
+wnb_gap=""
+wnb_gap_rate==""
+rb_gap_rate=""
 
-for usb3_info in ${usb3_infos}; do
+echo "usb3_size,           nobounce,     nobounce_strict,     bounce"
+echo "usb3_size,           nobounce,     nobounce_strict,     bounce" > $LOG_PATH/$read_file
+echo "usb3_size,           nobounce,     nobounce_strict,     bounce" > $LOG_PATH/$write_file
+
+for usb3 in ${usb3_infos}; do
+	num=$((num+1))
 	read_result=""
+	block="${usb3}k*1024"
+	usb3_info=" $usb3 MB"
 	rb_value=$(cat $bounce_usb3 | grep "$usb3_info" | awk -F 'read ' '{print $2}' | awk -F ' MB' '{print $1}')
 	rnb_value=$(cat $nobounce_usb3 | grep "$usb3_info" | awk -F 'read ' '{print $2}' | awk -F ' MB' '{print $1}')
-	read_gap=$(printf "%.2f" $(echo "scale=2;$rnb_value - $rb_value" | bc))
-	read_gap_rate=$(printf "%.4f" $(echo "scale=4;$read_gap/$rnb_value" | bc))
-	read_result=$(printf "%-10s  read:%-10s, %-10s, %16s" "${usb3_dict[$usb3_info]}" "$rnb_value" "$rb_value" "$read_gap_rate")
+	rnbs_value=$(cat $nbs_usb3 | grep "$usb3_info" | awk -F 'read ' '{print $2}' | awk -F ' MB' '{print $1}')
+	read_result=$(printf "%-13s  read,%-10s, %-10s, %16s" "$block" "$rnb_value" "$rnbs_value" "$rb_value")
 	rb_total=$(printf "%.2f" $(echo "scale=2;$rb_total + $rb_value" | bc))
 	rnb_total=$(printf "%.2f" $(echo "scale=2;$rnb_total + $rnb_value" | bc))
+	rnbs_total=$(printf "%.2f" $(echo "scale=2;$rnbs_total + $rnbs_value" | bc))
 	echo "$read_result"
-
+	echo "$read_result" >> $LOG_PATH/$read_file
 
 	write_result=""
 	wb_value=$(cat $bounce_usb3 | grep "$usb3_info" | awk -F 'write ' '{print $2}' | awk -F ' MB' '{print $1}')
 	wnb_value=$(cat $nobounce_usb3 | grep "$usb3_info" | awk -F 'write ' '{print $2}' | awk -F ' MB' '{print $1}')
-	write_gap=$(printf "%.2f" $(echo "scale=2;$wnb_value - $wb_value" | bc))
-	write_gap_rate=$(printf "%.4f" $(echo "scale=4;$write_gap/$wnb_value" | bc))
-	write_result=$(printf "%-10s write:%-10s, %-10s, %16s" "${usb3_dict[$usb3_info]}" "$wnb_value" "$wb_value" "$write_gap_rate")
+	wnbs_value=$(cat $nbs_usb3 | grep "$usb3_info" | awk -F 'write ' '{print $2}' | awk -F ' MB' '{print $1}')
+	write_result=$(printf "%-13s write,%-10s, %-10s, %16s" "$block" "$wnb_value" "$wnbs_value" "$wb_value")
 	wb_total=$(printf "%.2f" $(echo "scale=2;$wb_total + $wb_value" | bc))
 	wnb_total=$(printf "%.2f" $(echo "scale=2;$wnb_total + $wnb_value" | bc))
+	wnbs_total=$(printf "%.2f" $(echo "scale=2;$wnbs_total + $wnbs_value" | bc))
 	echo "$write_result"
+	echo "$write_result" >> $LOG_PATH/$write_file
+
 done
-	rb_total=$(printf "%.2f" $(echo "scale=2;$rb_total/5" | bc))
-	rnb_total=$(printf "%.2f" $(echo "scale=2;$rnb_total/5" | bc))
+	echo "sample num:$num"
+	rb_total=$(printf "%.3f" $(echo "scale=3;$rb_total/$num" | bc))
+	rnb_total=$(printf "%.3f" $(echo "scale=3;$rnb_total/$num" | bc))
+	rnbs_total=$(printf "%.3f" $(echo "scale=3;$rnbs_total/$num" | bc))
 
-	rb_gap=$(printf "%.2f" $(echo "scale=2;$rnb_total - $rb_total" | bc))
-	rb_gap_rate=$(printf "%.4f" $(echo "scale=4;$rb_gap/$rnb_total" | bc))
-	echo "usb3 read nobounce average MB/s:$rnb_total,  bounce average MB/s:$rb_total, gap rate:$rb_gap_rate"
+	rb_gap=$(printf "%.3f" $(echo "scale=3;$rb_total - $rnbs_total" | bc))
+	rb_gap_rate=$(printf "%.4f" $(echo "scale=4;$rb_gap/$rnbs_total" | bc))
+	
+	rnb_gap=$(printf "%.3f" $(echo "scale=3;$rnbs_total - $rnb_total" | bc))
+	rnb_gap_rate=$(printf "%.4f" $(echo "scale=4;$rnb_gap/$rnb_total" | bc))
 
-	wb_total=$(printf "%.2f" $(echo "scale=2;$wb_total/5" | bc))
-	wnb_total=$(printf "%.2f" $(echo "scale=2;$wnb_total/5" | bc))
+	echo "usb3 read nobounce average MB/s:$rnb_total, nobounce strict(delay iotlb flush disabled) average MB/s:$rnbs_total, bounce average:$rb_total"
+	echo "read nobounce with strict gap rate:$rnb_gap_rate,   nobounce strict and bounce gap rate:$rb_gap_rate"
 
-	wb_gap=$(printf "%.2f" $(echo "scale=2;$wnb_total - $wb_total" | bc))
-	wb_gap_rate=$(printf "%.4f" $(echo "scale=4;$wb_gap/$wnb_total" | bc))
+	wb_total=$(printf "%.3f" $(echo "scale=3;$wb_total/$num" | bc))
+	wnb_total=$(printf "%.3f" $(echo "scale=3;$wnb_total/$num" | bc))
+	wnbs_total=$(printf "%.3f" $(echo "scale=3;$wnbs_total/$num" | bc))
 
-	echo "usb3 write nobounce average MB/s:$wnb_total,  bounce average MB/s:$wb_total, gap rate:$wb_gap_rate"
+	wb_gap=$(printf "%.3f" $(echo "scale=3;$wb_total - $wnbs_total" | bc))
+	wb_gap_rate=$(printf "%.4f" $(echo "scale=4;$wb_gap/$wnbs_total" | bc))
+
+	wnb_gap=$(printf "%.3f" $(echo "scale=3;$wnbs_total - $wnb_total" | bc))
+	wnb_gap_rate=$(printf "%.4f" $(echo "scale=4;$wnb_gap/$wnb_total" | bc))
+
+	echo "usb3 write nobounce average MB/s:$wnb_total, nobounce strict(delay iotlb flush disabled) average MB/s:$wnbs_total, bounce average:$wb_total"
+	echo "write nobounce with strict gap rate:$wnb_gap_rate,   nobounce strict and bounce gap rate:$wb_gap_rate"
