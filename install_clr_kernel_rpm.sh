69062cc2b0668e3c97efc38ac3c62d6184fcbff8
Pengfei Xu <pengfei.xu@intel.com>
2019-03-13 14:28:32 +0800

    install_clr_kernel_rpm.sh: install clear OS kernel script
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ install_clr_kernel_rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+home=$(pwd)
+build=$(ls -1 kernel-*rpm | head -n 1)
+[[ -n "$build" ]] || {
+	        echo "no build.rpm:$build"
+        return 1
+}
+
+echo "build:$build"
+echo "rpm2cpio $build | cpio -idmv"
+rpm2cpio $build | cpio -idmv
+module=$(ls ${home}/lib/modules/)
+[[ -n "$module" ]] || {
+	echo "lib/modules is null:$module"
+	return 1
+}
+echo "cp -r ${home}/lib/modules/$module /usr/lib/modules/"
+cp -r ${home}/lib/modules/$module /usr/lib/modules/
+sleep 1
+echo "cd ${home}/boot/"
+cd ${home}/boot/
+vmlinuz=$(ls ${home}/boot/vmlinuz*)
+system=$(ls ${home}/boot/System*)
+config=$(ls ${home}/boot/config*)
+echo "installkernel $module $vmlinuz $system /boot"
+installkernel $module $vmlinuz $system /boot
+echo "cp ${home}/boot/$config /lib/kernel/"
+cp ${home}/boot/$config /lib/kernel/
