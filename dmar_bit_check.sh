1b18d9077b9914e13bdd95a3ed54d1fbfcf6d5e8
Pengfei Xu <pengfei.xu@intel.com>
2019-03-25 16:44:06 +0800

    dmar_bit_check: solve need answer y issue
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- dmar_bit_check.sh
+++ dmar_bit_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dmar=""
 result=""
-echo "acpidump -f -o acpi.dump"
-acpidump -f -o acpi.dump
+echo "echo y | acpidump -o -f acpi.dump"
+echo y | acpidump -o acpi.dump
 echo "acpixtract -f -a acpi.dump"
-acpixtract -f -a acpi.dump
+acpixtract -a acpi.dump
 echo "iasl dmar.dat"
 iasl dmar.dat
 
