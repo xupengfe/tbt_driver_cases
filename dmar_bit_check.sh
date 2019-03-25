416ab0675346217c1865d2069fc769531a00b2b2
Pengfei Xu <pengfei.xu@intel.com>
2019-03-25 16:53:29 +0800

    dmar_bit_check: check dma sysfs file
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- dmar_bit_check.sh
+++ dmar_bit_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 dmar=""
 result=""
+dma_file="/sys/bus/thunderbolt/devices/domain0/iommu_dma_protection"
+dma_value=$(cat $dma_file)
 
 echo "echo y | acpidump -o -f acpi.dump"
 echo y | acpidump -o acpi.dump
@@ -12,6 +14,11 @@ iasl dmar.dat
 dmar=$(cat dmar.dsl | grep "025h")
 result=$(cat dmar.dsl | grep "025h" | grep "05")
 echo "dmar:$dmar"
+[[ "$dma_value" -eq 1 ]] && {
+	echo "Check $dma_file set to 1, passed!"
+	exit 0
+}
+
 [[ -n "$result" ]] || {
 	echo "Check failed!"
 	exit 1
