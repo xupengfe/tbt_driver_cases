8e317091efe39cd74b16c25033c22153bb8eceac
Pengfei Xu <pengfei.xu@intel.com>
2019-03-25 16:30:53 +0800

    dmar_bit_check: add print to check
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- dmar_bit_check.sh
+++ dmar_bit_check.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 dmar=""
 result=""
-
+echo "acpidump -f -o acpi.dump"
 acpidump -f -o acpi.dump
+echo "acpixtract -f -a acpi.dump"
 acpixtract -f -a acpi.dump
+echo "iasl dmar.dat"
 iasl dmar.dat
 
 dmar=$(cat dmar.dsl | grep "025h")
