fc911a1a7f831f2387598d2eabdff5262e851998
Pengfei Xu <pengfei.xu@intel.com>
2019-07-05 11:19:24 +0800

    generate_ini.sh: add compare function and help description
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- generate_ini.sh
+++ generate_ini.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-SOUREC_FILE=$1
-GEN_FILE=$2
+SOUREC_FILE=""
+GEN_FILE=""
 NAMES=""
-
-[[ -n "$GEN_FILE" ]] || {
-  echo "No Parm2, set bios.ini as default"
-  GEN_FILE="bios.ini"
-}
 
 usage() {
   cat <<__EOF
-  usage: ./${0##*/} ORIGIN_FILE TARGET_FILE
-    ORIGIN_FILE: usually it's default setting xml file
-    TARGET_FILE: usually it's our target worked bios xml file
-    Will generate bios_set.nsh
+  usage: ./${0##*/} [-f XML_FILE][-c CURRENT_INI][-t TARGET_INI]
+    -f XML_FILE: will generate full bios item ini
+    -c CURRENT_INI -t TARGET_INI: will compare both and generate delta target ini
 __EOF
-}
-
-[[ -e "$SOUREC_FILE" ]] || {
-  echo "No origin file:$SOUREC_FILE exist"
-  usage
-  exit 2
 }
 
 generate_ini() {
   name=""
   NAMES=$(cat $SOUREC_FILE | grep " name=\"" | awk -F "name=" '{print $2}' | awk -F '\"' '{print $2}')
   value=""
+
+  [[ -n "$GEN_FILE" ]] || {
+    echo "No -o setting, set bios.ini as default output ini file"
+    GEN_FILE="bios.ini"
+  }
 
   cat /dev/null > $GEN_FILE
   for name in ${NAMES}; do
@@ -40,4 +33,94 @@ generate_ini() {
   echo "Done. Generated $GEN_FILE"
 }
 
-generate_ini
+compare_ini() {
+  target_items=""
+  item=""
+  target_name=""
+  target_value=""
+  result_name=""
+
+  [[ -n "$GEN_FILE" ]] || {
+    echo "No -o setting, set bios.ini as default output ini file"
+    GEN_FILE="bios.ini"
+  }
+  cat /dev/null > $GEN_FILE
+
+  target_items=$(cat $TARGET_INI)
+  for item in $target_items; do
+    result=""
+    target_name=""
+    target_value=""
+    result_name=""
+    result_value=""
+
+    target_name=$(echo $item | cut -d '=' -f 1)
+    target_value=$(echo $item | cut -d '=' -f 2)
+    result_name=$(cat $CURRENT_INI | grep $target_name)
+
+    [[ -n "$result_name" ]] || {
+      echo "item $target_name find in target $TARGET_INI but no in $CURRENT_INI"
+      continue
+    }
+    result_value=$(cat $CURRENT_INI | grep $target_name | grep $target_value)
+    if [[ -z "$result_value" ]]; then
+      echo "Item:$result_name will change to $target_value"
+    else
+      continue
+    fi
+    echo $item >> $GEN_FILE
+
+  done
+}
+
+
+while getopts :f:c:t:o:h arg
+do
+  case $arg in
+    f)
+      SOUREC_FILE=$OPTARG
+      [[ -e "$SOUREC_FILE" ]] || {
+        echo "No origin file:$SOUREC_FILE exist"
+        usage
+        exit 2
+      }
+      generate_ini
+      ;;
+    c)
+      CURRENT_INI=$OPTARG
+      [[ -e "$CURRENT_INI" ]] || {
+        echo "No current ini file:$CURRENT_INI exist"
+        usage
+        exit 2
+      }
+      ;;
+    t)
+      TARGET_INI=$OPTARG
+      [[ -e "$TARGET_INI" ]] || {
+        echo "No target ini file:$TARGET_INI exist"
+        usage
+        exit 2
+      }
+      if [[ -z "$CURRENT_INI" ]]; then
+        echo "No current ini:$CURRENT_INI"
+        usage
+        exit 2
+      else
+        compare_ini
+      fi
+      ;;
+     o)
+       GEN_FILE=$OPTARG
+       ;;
+     h)
+       usage
+       exit 2
+       ;;
+     :)
+       usage
+       exit 2
+       ;;
+  esac
+done
+
+[[ -z "$SOUREC_FILE" ]] && [[ -z "$CURRENT_INI" ]] && usage
