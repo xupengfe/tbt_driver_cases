6f950bd3f146ada42f804a759d75c0e07951939a
root <root@p-cml03.sh.intel.com>
2019-07-20 03:45:32 +0000

    1.sh: correct it
    Signed-off-by: root <root@p-cml03.sh.intel.com>

--- 1.sh
+++ 1.sh
@@ -2,4 +2,11 @@
 
 #cat  read_*.CSV
 #cat  write_*.CSV
-eval "t=($(nmcli device | grep ethernet | awk '{print $1}' | xargs))"; for i in ${t[@]}; do sudo ifconfig $i up; done;
+eth_ports=$(nmcli device | grep ethernet | awk '{print $1}' | xargs)
+echo "eth_ports: $eth_ports"
+
+for eth in "$eth_ports"; do
+  echo "sudo ifconfig $eth up"
+  sudo ifconfig $eth up
+done
+#eval "t=($(nmcli device | grep ethernet | awk '{print $1}' | xargs))"; for i in ${t[@]}; do sudo ifconfig $i up; done;
