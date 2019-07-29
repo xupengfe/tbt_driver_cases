cd854cd50219971ef0200874c173d7f1730e8c0d
Pengfei Xu <pengfei.xu@intel.com>
2019-07-29 14:17:27 +0800

    1.sh: solve mainline usb ethernet card not found issue
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- 1.sh
+++ 1.sh
@@ -1,2 +1,5 @@
-cat  read_*.CSV
-cat  write_*.CSV
+#!/bin/bash
+
+#cat  read_*.CSV
+#cat  write_*.CSV
+eval "t=($(nmcli device | grep ethernet | awk '{print $1}' | xargs))"; for i in ${t[@]}; do sudo ifconfig $i up; done;
