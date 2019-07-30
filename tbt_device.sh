75363c774ec83d1013a71a6da944ec757c85bb21
Pengfei Xu <pengfei.xu@intel.com>
2019-07-30 16:23:14 +0000

    tbt_device: caculate tbt devices number
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- tbt_device.sh
+++ tbt_device.sh
@@ -6,6 +6,7 @@ tbt_device_check() {
   local pci=""
   local is_tbt_pci=""
   local pci_device=""
+  local tbt_device_num=0
 
   for pci in $pcis; do
     is_tbt_pci=""
@@ -14,11 +15,13 @@ tbt_device_check() {
     if [[ -z "$is_tbt_pci" ]]; then
       continue
     else
+      tbt_device_num=$((tbt_device_num + 1))
       pci_device=$(lspci | grep "$pci" | awk -F "Device" '{print $2}' | cut -d ' ' -f 2)
       echo "tbt pci:$pci  device_id:$pci_device"
       [[ -n "$pci_device" ]] || echo "tbt device id is null->$(lspci | grep "$pci")"
     fi
   done
+  echo "tbt device num:$tbt_device_num"
 }
 
 tbt_device_check
