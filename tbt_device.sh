c2b239b1a172c4bf8c53227e15413c83a5af5d53
Pengfei Xu <pengfei.xu@intel.com>
2019-07-30 15:46:02 +0000

    tbt_device.sh: show tbt pci device id
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ tbt_device.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+tbt_device_check() {
+  local pcis=$(lspci |  cut -d ' ' -f 1)
+  local pci=""
+  local is_tbt_pci=""
+  local pci_device=""
+
+  for pci in $pcis; do
+    is_tbt_pci=""
+    pci_device=""
+    is_tbt_pci=$(lspci -vv -s "$pci"| grep "thunderbolt")
+    if [[ -z "$is_tbt_pci" ]]; then
+      continue
+    else
+      pci_device=$(lspci | grep "$pci" | awk -F "Device" '{print $2}' | cut -d ' ' -f 2)
+      echo "tbt pci:$pci device:$pci_device"
+    fi
+  done
+}
+
+tbt_device_check
