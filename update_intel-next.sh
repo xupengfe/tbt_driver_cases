9ff18bcfd3aa8014d0dc2198fc3052ae7df4afcf
Pengfei Xu <pengfei.xu@intel.com>
2019-11-12 12:20:05 +0000

    update_intel-next.sh: improve it
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- update_intel-next.sh
+++ update_intel-next.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /root/otc_intel_next-linux
+log_path="/root/update_intel-next.log"
+path_file="/root/path"
+code_path=""
+time=$(date +%Y-%m-%d_%H:%M)
+
+code_path=$(cat $path_file)
+[[ -n "$code_path" ]] || code_path="/root/otc_intel_next-linux"
+
+cd $code_path
 git fetch origin
+echo "$time: $code_path, 'git fetch origin' result:$?" >> $code_path
