fb9e5f9d3b7c8edfc4e3f7aee0efdab8bca76af0
Pengfei Xu <pengfei.xu@intel.com>
2022-07-13 13:44:32 +0800

    usb4switch: it's for usb4 switch 3141 controller
    USB4 swich 3141 link:
    https://mcci.com/usb/dev-tools/model-3141/
    It will replace cleware.
    
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ usb4switch3141/usb4switch.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+serial_cmd() {
+  local cmd_file=$1
+
+  {
+    sleep 2;
+    while pgrep runscript > /dev/null; do
+      sleep 2;
+    done;
+    cat escape.txt;
+  } | minicom -b 9600 -D /dev/ttyACM0 -S $cmd_file -C capture.txt
+}
+
+
+hot_plug() {
+  local p0=""
+  local p1=""
+  local p2=""
+
+  p0=$(serial_cmd "status" | grep "PORTF")
+  echo "p0:$p0"
+  serial_cmd "superspeed"
+  serial_cmd "port1"
+  p1=$(serial_cmd "status" | grep "PORTF")
+  echo "p1:$p1"
+  serial_cmd "superspeed"
+  serial_cmd "port2"
+  p2=$(serial_cmd "status" | grep "PORTF")
+  echo "p2:$p2"
+  serial_cmd "superspeed"
+  serial_cmd "port0"
+
+  echo "p0:$p0"
+  echo "p1:$p1"
+  echo "p2:$p2"
+}
+
+hot_plug
