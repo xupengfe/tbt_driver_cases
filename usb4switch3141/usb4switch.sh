056632ee10ef4a6cb218b0f857a9f3b9a720f276
Pengfei Xu <pengfei.xu@intel.com>
2022-07-18 11:39:09 +0800

    usb4switch: add plug out/in and status check functions
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- usb4switch3141/usb4switch.sh
+++ usb4switch3141/usb4switch.sh
@@ -4,14 +4,50 @@ serial_cmd() {
   local cmd_file=$1
 
   {
-    sleep 2;
+    sleep 1;
     while pgrep runscript > /dev/null; do
-      sleep 2;
+      sleep 1;
     done;
     cat escape.txt;
   } | minicom -b 9600 -D /dev/ttyACM0 -S $cmd_file -C capture.txt
 }
 
+check_status() {
+  local state=""
+
+  state=$(serial_cmd "status" | grep "PORTF" 2>/dev/null)
+  if [[ -z "$state" ]]; then
+    echo "Seems no USB4 switch 3141 connected state:$state"
+  else
+    echo "USB4 switch 3141 connected, state:$state"
+  fi
+}
+
+plug_in() {
+  local plug_state=""
+
+  plug_state=$(serial_cmd "status" | grep "PORTF: 0x12" 2>/dev/null)
+  if [[ -n "$plug_state" ]]; then
+    echo "Already connected port 1 for USB4 switch:$plug_state"
+  else
+    echo "plug_state:$plug_state"
+    serial_cmd "superspeed"
+    serial_cmd "port1"
+  fi
+}
+
+plug_out() {
+  local plug_state=""
+
+  plug_state=$(serial_cmd "status" | grep "PORTF: 0x70" 2>/dev/null)
+  if [[ -n "$plug_state" ]]; then
+    echo "Already connected port 1 for USB4 switch:$plug_state"
+  else
+    echo "plug_state:$plug_state"
+    serial_cmd "superspeed"
+    serial_cmd "port0"
+  fi
+}
 
 hot_plug() {
   local p0=""
@@ -37,4 +73,18 @@ hot_plug() {
   echo "p2:$p2"
 }
 
-hot_plug
+parm=$1
+case $parm in
+  s)
+    check_status
+    ;;
+  on)
+    plug_in
+    ;;
+  off)
+    plug_out
+    ;;
+  *)
+    hot_plug
+    ;;
+esac
