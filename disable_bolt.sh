23695c80cdb39b9469f49f8c898943dd96b83329
Pengfei Xu <pengfei.xu@intel.com>
2023-12-04 13:41:24 +0800

    disable_bolt: used the foget -a way instead
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- disable_bolt.sh
+++ disable_bolt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-systemctl disable bolt.service 2>/dev/null
-systemctl stop  bolt.service 2>/dev/null
-systemctl status  bolt.service
-mv /usr/lib/systemd/system/bolt.service   /root
-mv /usr/libexec/boltd   /root
-ps -ef | grep bolt
+echo "boltctl forget -a"
+boltctl forget -a
+#systemctl disable bolt.service 2>/dev/null
+#systemctl stop  bolt.service 2>/dev/null
+#systemctl status  bolt.service
+#mv /usr/lib/systemd/system/bolt.service   /root
+#mv /usr/libexec/boltd   /root
+#ps -ef | grep bolt
